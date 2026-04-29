63985a4995daebbf4ef4e68589aa453b4c3571aa
steppinio <jan.steppe@outlook.de>
2026-04-29 15:36:02 +0000

    fix: linting argument_list.js

--- workercoopwissen/workercoop_wissen/doctype/argument/argument_list.js
+++ workercoopwissen/workercoop_wissen/doctype/argument/argument_list.js
@@ -1,4 +1,4 @@
 // ID in der Argument List verstecken
-frappe.listview_settings['Argument'] = {
-    hide_name_column: true
+frappe.listview_settings["Argument"] = {
+    hide_name_column: true,
 };
