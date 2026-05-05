dd163156a83673bb5a93f611db3d6b8f7e493d15
steppinio <jan.steppe@outlook.de>
2026-05-05 18:37:45 +0000

    fix: workspacer aus hooks.py entfernt

--- workercoopwissen/hooks.py
+++ workercoopwissen/hooks.py
@@ -11,14 +11,14 @@ app_license = "gpl-3.0"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-add_to_apps_screen = [
-	{
-		"name": "workercoopwissen",
-		"logo": "/assets/workercoopwissen/logo.png",
-		"title": "WorkerCoop Wissen",
-		"route": "/workercoopwissen",
-	}
-]
+#add_to_apps_screen = [
+#	{
+#		"name": "workercoopwissen",
+#		"logo": "/assets/workercoopwissen/logo.png",
+#		"title": "WorkerCoop Wissen",
+#		"route": "/workercoopwissen",
+#	}
+#]
 # 		"has_permission": "workercoopwissen.api.permission.has_app_permission"
 
 # Includes in <head>
