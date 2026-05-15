b3a3e92b6930aa31746f8db25ce78fd6aef84f93
steppinio <jan.steppe@outlook.de>
2026-05-15 21:32:58 +0000

    feat: Coop-Nerd Rolle, Workflow, Permissions als Fixtures; List View aufgeräumt

--- workercoopwissen/workercoop_wissen/doctype/worker_coop/worker_coop.py
+++ workercoopwissen/workercoop_wissen/doctype/worker_coop/worker_coop.py
@@ -6,4 +6,23 @@ from frappe.model.document import Document
 
 
 class WorkerCoop(Document):
-	pass
+	def validate(self): # self // serverseitige Referenz für aktuelle Dokument
+		wm = self.worker_members or 0
+		nmw= self.non_member_workers or 0
+		nwvm = self.non_worker_voting_members or 0
+		
+		self.total_workforce = wm + nmw
+		self.total_members = wm + nwvm
+
+		# Anteil Worker-Members an Belegschschaft
+		if self.total_workforce > 0:
+			self.worker_member_ratio = (wm / self.total_workforce) * 100
+		else:
+			self.worker_member_ratio = 0
+		
+		# Anteil Worker-Members an Gesamtmitgliedern
+		if self.total_members > 0:
+			self.member_worker_ratio = (wm / self.total_members) * 100
+		else:
+			self.member_worker_ratio = 0
+
---
+++ workercoopwissen/workercoop_wissen/doctype/worker_coop/worker_coop_list.js
@@ -0,0 +1,4 @@
+frappe.listview_settings["Worker Coop"] = {
+    hide_name_column: true,
+    add_fields: ["workflow_state", "legal_form", "total_workforce", "website", "city"]
+};
